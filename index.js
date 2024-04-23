56099b421e5041620e642b86faafb86f8b3c55bf
im-sanny <raselparvezsanny@gmail.com>
2024-04-23 10:43:20 +0600

    user added in firebase and mongodb

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ async function run() {
     await client.connect();
 
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
+    const userCollection = client.db("coffeeDB").collection("user");
+
+    app.get("coffee", async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     app.get("/coffee", async (req, res) => {
       const cursor = coffeeCollection.find();
@@ -53,7 +60,7 @@ async function run() {
       const filter = { _id: new ObjectId(id) };
       const option = { upsert: true };
       const updatedCoffee = req.body;
-      
+
       const coffee = {
         $set: {
           name: updatedCoffee.name,
@@ -65,14 +72,28 @@ async function run() {
           photo: updatedCoffee.photo,
         },
       };
-      const result = await coffeeCollection.updateOne(filter, coffee, option)
-      res.send(result)
+      const result = await coffeeCollection.updateOne(filter, coffee, option);
+      res.send(result);
     });
 
     app.delete("/coffee/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //user related apis
+    app.get("/user", async (req, res) => {
+      const cursor = userCollection.find();
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
